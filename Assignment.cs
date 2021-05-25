7304489ed3d172a422b52c6be0e9563f98b53163
tanveshrele <48552975+tanveshrele@users.noreply.github.com>
2021-05-25 09:49:52 +0530

    Add files via upload

---
+++ Assignment.cs
@@ -0,0 +1,41 @@
+﻿using Assignment.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment.Controllers
+{
+    public class Assignment : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+        public IActionResult RegistrationForm()
+        {
+            return View(new Person());
+        }
+        public IActionResult DisplayForm(Person person)
+        {
+            if (person != null)
+            {
+                ViewBag.CommonMessage = "Thank You  " + person.Name;
+                if (person.WillAttend == "yes")
+                {
+                    ViewBag.message = "Thank You for Coming";
+                }
+                if (person.WillAttend == "no")
+                {
+                    ViewBag.message = "Sorry  to hear you";
+                }
+            }
+            return View();
+                
+            }
+
+
+        }
+    }
+
---
+++ Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment.Models
+{
+    public class Person
+    {
+        [Required(ErrorMessage = "Name is compulsory field")]
+        [StringLength(10, ErrorMessage = "Maximum Name length is 10 character")]
+        public string Name { get; set; }
+
+
+
+        [Required(ErrorMessage = "mobileno is compulsory field")]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number")]
+        public int mobileno { get; set; }
+
+
+
+        [Required(ErrorMessage = "Email-id is compulsory field")]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
+        public string Emailid { get; set; }
+
+
+
+        [Required(ErrorMessage = "list is compulsory field")]
+        public string WillAttend { get; set; }
+    }
+}
